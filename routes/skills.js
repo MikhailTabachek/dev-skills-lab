d13c1a41d5fcb009fb6e82f3572fca510c918b1f
Mikhail <mishanyaveselyi@gmail.com>
2022-02-24 19:44:25 -0500

    Built Controller

--- routes/skills.js
+++ routes/skills.js
@@ -1,17 +1,9 @@
 import { Router } from 'express'
-import * as skillsDb from "../data/skills-db.js"
+import * as skillsCtrl from "../controllers/skills.js"
 const router = Router()
 
 /* GET skills listing. */
-router.get('/', function(req, res) {
-  skillsDb.find({}, function(error, skills) {
-    res.render('skills/index', {
-      skills: skills,
-      error: error
-    })
-  })
-})
-
+router.get('/', skillsCtrl.index)
 
 export {
   router
